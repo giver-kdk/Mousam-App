266845905ebe18e8fc0e5694fb97806c9d2ad042
giver-kdk <giverbro13@gmail.com>
2024-06-04 14:40:11 +0545

    Map Added

---
+++ wwwroot/js/weather-logics.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let searcher = document.getElementById("location__search");
+    let search = document.getElementById("search");
+    let closeSearch = document.querySelector("#search .close__icon");
+    let errorBox = document.querySelector(".error__alert");
+    let closeError = document.querySelector(".error__alert .close__icon");
+    let searchBtn = document.getElementById("search__btn");
+
+    searcher.addEventListener("click", function () {
+        search.classList.toggle("active");
+    });
+
+    searchBtn.addEventListener("click", function () {
+        // Add your search logic here (optional)
+        search.classList.remove("active");
+    });
+
+    closeSearch.addEventListener("click", function () {
+        // Add your search logic here (optional)
+        search.classList.remove("active");
+    });
+
+    closeError.addEventListener("click", function () {
+        // Add your search logic here (optional)
+        errorBox.classList.remove("active");
+    });
+});
